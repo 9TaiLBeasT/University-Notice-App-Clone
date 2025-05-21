4ebcb72625a0a202d1aa042da6097aa58d38ecb7
9TaiLBeasT <gtsganesh2005@gmail.com>
2025-05-21 14:00:58 +0530

    first commit

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -3,8 +3,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    // Either use URL with embedded credentials:
-    private static final String URL = "postgresql://postgres.jxahlvgkxxmwnwoxaxja:[Ganesh123@]@aws-0-ap-south-1.pooler.supabase.com:6543/postgres";
+    private static final String URL = "jdbc:postgresql://aws-0-ap-south-1.pooler.supabase.com:6543/postgres";
     private static final String USER = "postgres.jxahlvgkxxmwnwoxaxja";
     private static final String PASSWORD = "Ganesh123@";
 
